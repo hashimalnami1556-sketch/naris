35aee99d9b010a51382f265ac5494cdec000ea0b
hashimalnami1556-sketch <hashimalnami1556@gmail.com>
2026-08-10 12:25:02 +0300

    fix: remove hardcoded phase thresholds from phase component

--- unreal/NARIS_W04/Source/NARIS_W04/Private/BoneBeastPhaseComponent.cpp
+++ unreal/NARIS_W04/Source/NARIS_W04/Private/BoneBeastPhaseComponent.cpp
@@ -1,28 +1,12 @@
 #include "BoneBeastPhaseComponent.h"
 
-void UBoneBeastPhaseComponent::EvaluateHealth(float CurrentHealth, float MaxHealth)
+void UBoneBeastPhaseComponent::SetAuthoritativePhase(ENarisBossPhase NewPhase)
 {
-    if (MaxHealth <= 0.f)
+    if (NewPhase == MirroredPhase)
     {
         return;
     }
 
-    const float Ratio = CurrentHealth / MaxHealth;
-    int32 PhaseIndex = 0;
-
-    // Generic three-phase scaffold. Final thresholds belong in BossData/playtest.
-    if (Ratio <= 0.66f)
-    {
-        PhaseIndex = 1;
-    }
-    if (Ratio <= 0.33f)
-    {
-        PhaseIndex = 2;
-    }
-
-    if (PhaseIndex != LastPhaseIndex)
-    {
-        LastPhaseIndex = PhaseIndex;
-        OnPhaseTransition.Broadcast(FName(*FString::Printf(TEXT("Phase%d"), PhaseIndex + 1)));
-    }
+    MirroredPhase = NewPhase;
+    OnPhaseTransition.Broadcast(FName(*UEnum::GetValueAsString(NewPhase)));
 }
