09b6f27fdbb9ce88ebed4f5b3f85037853d5d8c9
hashimalnami1556-sketch <hashimalnami1556@gmail.com>
2026-09-23 16:02:44 +0300

    Add essence cycling and localized display names

--- unreal/NARIS_W04/Source/NARIS_W04/Public/NarisEnergyComponent.h
+++ unreal/NARIS_W04/Source/NARIS_W04/Public/NarisEnergyComponent.h
@@ -1,18 +1,39 @@
 #pragma once
+
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "NarisGameplayTypes.h"
 #include "NarisEnergyComponent.generated.h"
 
 UCLASS(ClassGroup=(NARIS), Blueprintable, meta=(BlueprintSpawnableComponent))
-class NARIS_W04_API UNarisEnergyComponent : public UActorComponent {
+class NARIS_W04_API UNarisEnergyComponent : public UActorComponent
+{
     GENERATED_BODY()
+
 public:
     UNarisEnergyComponent();
-    UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Energy") float MaxEnergy=100.f;
-    UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Energy") float Energy=100.f;
-    UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Energy") ENarisEssence ActiveEssence=ENarisEssence::NarisFlame;
-    UFUNCTION(BlueprintCallable,Category="NARIS|Energy") bool Spend(float Amount);
-    UFUNCTION(BlueprintCallable,Category="NARIS|Energy") void Restore(float Amount);
-    UFUNCTION(BlueprintCallable,Category="NARIS|Energy") void SetEssence(ENarisEssence NewEssence);
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Energy")
+    float MaxEnergy = 100.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Energy")
+    float Energy = 100.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Energy")
+    ENarisEssence ActiveEssence = ENarisEssence::NarisFlame;
+
+    UFUNCTION(BlueprintCallable, Category="NARIS|Energy")
+    bool Spend(float Amount);
+
+    UFUNCTION(BlueprintCallable, Category="NARIS|Energy")
+    void Restore(float Amount);
+
+    UFUNCTION(BlueprintCallable, Category="NARIS|Energy")
+    void SetEssence(ENarisEssence NewEssence);
+
+    UFUNCTION(BlueprintCallable, Category="NARIS|Energy")
+    void CycleEssence(int32 Direction);
+
+    UFUNCTION(BlueprintPure, Category="NARIS|Energy")
+    FText GetActiveEssenceDisplayName() const;
 };
