8db9f5c9ece8fee7299f437809438d5069cb5e93
hashimalnami1556-sketch <hashimalnami1556@gmail.com>
2026-09-23 15:03:42 +0300

    Bind hero Interact action to interaction component

--- unreal/NARIS_W04/Source/NARIS_W04/Private/NarisHeroCharacter.cpp
+++ unreal/NARIS_W04/Source/NARIS_W04/Private/NarisHeroCharacter.cpp
@@ -1,39 +1,138 @@
 #include "NarisHeroCharacter.h"
-#include "NarisCombatComponent.h"
-#include "NarisEnergyComponent.h"
-#include "NarisLockOnComponent.h"
-#include "NarisInteractionComponent.h"
+
 #include "Camera/CameraComponent.h"
-#include "GameFramework/SpringArmComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/Controller.h"
-#include "GameFramework/InputSettings.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "NarisCombatComponent.h"
+#include "NarisEnergyComponent.h"
+#include "NarisInteractionComponent.h"
+#include "NarisLockOnComponent.h"
+
 ANarisHeroCharacter::ANarisHeroCharacter()
 {
- PrimaryActorTick.bCanEverTick=true;
- Combat=CreateDefaultSubobject<UNarisCombatComponent>(TEXT("Combat"));
- Energy=CreateDefaultSubobject<UNarisEnergyComponent>(TEXT("Energy"));
- LockOn=CreateDefaultSubobject<UNarisLockOnComponent>(TEXT("LockOn"));
- Interaction=CreateDefaultSubobject<UNarisInteractionComponent>(TEXT("Interaction"));
- CameraBoom=CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom")); CameraBoom->SetupAttachment(RootComponent); CameraBoom->TargetArmLength=340.f; CameraBoom->bUsePawnControlRotation=true;
- FollowCamera=CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera")); FollowCamera->SetupAttachment(CameraBoom); FollowCamera->bUsePawnControlRotation=false;
- GetCharacterMovement()->MaxWalkSpeed=WalkSpeed; GetCharacterMovement()->bOrientRotationToMovement=true; bUseControllerRotationYaw=false;
-}
-void ANarisHeroCharacter::Tick(float D){Super::Tick(D);if(Combat&&Combat->Health<=0)DisableInput(nullptr);}
-void ANarisHeroCharacter::SetupPlayerInputComponent(UInputComponent* I)
-{
- Super::SetupPlayerInputComponent(I);
+    PrimaryActorTick.bCanEverTick = true;
 
- I->BindAxis(TEXT("MoveForward"),this,&ANarisHeroCharacter::MoveForward); I->BindAxis(TEXT("MoveRight"),this,&ANarisHeroCharacter::MoveRight); I->BindAxis(TEXT("LookYaw"),this,&ANarisHeroCharacter::LookYaw); I->BindAxis(TEXT("LookPitch"),this,&ANarisHeroCharacter::LookPitch);
- I->BindAction(TEXT("LightAttack"),IE_Pressed,this,&ANarisHeroCharacter::LightAttack); I->BindAction(TEXT("HeavyAttack"),IE_Pressed,this,&ANarisHeroCharacter::HeavyAttack); I->BindAction(TEXT("Dodge"),IE_Pressed,this,&ANarisHeroCharacter::Dodge); I->BindAction(TEXT("Parry"),IE_Pressed,this,&ANarisHeroCharacter::Parry); I->BindAction(TEXT("ResonanceBurst"),IE_Pressed,this,&ANarisHeroCharacter::ResonanceBurst);
+    Combat = CreateDefaultSubobject<UNarisCombatComponent>(TEXT("Combat"));
+    Energy = CreateDefaultSubobject<UNarisEnergyComponent>(TEXT("Energy"));
+    LockOn = CreateDefaultSubobject<UNarisLockOnComponent>(TEXT("LockOn"));
+    Interaction = CreateDefaultSubobject<UNarisInteractionComponent>(TEXT("Interaction"));
+
+    CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+    CameraBoom->SetupAttachment(RootComponent);
+    CameraBoom->TargetArmLength = 340.f;
+    CameraBoom->bUsePawnControlRotation = true;
+
+    FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+    FollowCamera->SetupAttachment(CameraBoom);
+    FollowCamera->bUsePawnControlRotation = false;
+
+    GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
+    GetCharacterMovement()->bOrientRotationToMovement = true;
+    bUseControllerRotationYaw = false;
 }
-void ANarisHeroCharacter::MoveForward(float V){if(Controller&&V){FRotator R(0,Controller->GetControlRotation().Yaw,0);AddMovementInput(FRotationMatrix(R).GetUnitAxis(EAxis::X),V);}}
-void ANarisHeroCharacter::MoveRight(float V){if(Controller&&V){FRotator R(0,Controller->GetControlRotation().Yaw,0);AddMovementInput(FRotationMatrix(R).GetUnitAxis(EAxis::Y),V);}}
-void ANarisHeroCharacter::LookYaw(float V){AddControllerYawInput(V);}
-void ANarisHeroCharacter::LookPitch(float V){AddControllerPitchInput(V);}
-void ANarisHeroCharacter::LightAttack(){if(Combat)Combat->AddResonance(5.f);}
-void ANarisHeroCharacter::HeavyAttack(){if(Combat)Combat->AddResonance(10.f);}
-void ANarisHeroCharacter::Dodge(){GetCharacterMovement()->Velocity+=GetActorForwardVector()*650.f;}
-void ANarisHeroCharacter::Parry(){if(Combat)Combat->AddResonance(15.f);}
-void ANarisHeroCharacter::ResonanceBurst(){if(Combat&&Combat->Resonance>=Combat->MaxResonance){Combat->Resonance=0;}}
-void ANarisHeroCharacter::SetSprinting(bool B){GetCharacterMovement()->MaxWalkSpeed=B?SprintSpeed:WalkSpeed;}
+
+void ANarisHeroCharacter::Tick(float DeltaSeconds)
+{
+    Super::Tick(DeltaSeconds);
+
+    if (Combat && Combat->Health <= 0.f)
+    {
+        DisableInput(nullptr);
+    }
+}
+
+void ANarisHeroCharacter::SetupPlayerInputComponent(UInputComponent* Input)
+{
+    Super::SetupPlayerInputComponent(Input);
+
+    Input->BindAxis(TEXT("MoveForward"), this, &ANarisHeroCharacter::MoveForward);
+    Input->BindAxis(TEXT("MoveRight"), this, &ANarisHeroCharacter::MoveRight);
+    Input->BindAxis(TEXT("LookYaw"), this, &ANarisHeroCharacter::LookYaw);
+    Input->BindAxis(TEXT("LookPitch"), this, &ANarisHeroCharacter::LookPitch);
+
+    Input->BindAction(TEXT("LightAttack"), IE_Pressed, this, &ANarisHeroCharacter::LightAttack);
+    Input->BindAction(TEXT("HeavyAttack"), IE_Pressed, this, &ANarisHeroCharacter::HeavyAttack);
+    Input->BindAction(TEXT("Dodge"), IE_Pressed, this, &ANarisHeroCharacter::Dodge);
+    Input->BindAction(TEXT("Parry"), IE_Pressed, this, &ANarisHeroCharacter::Parry);
+    Input->BindAction(TEXT("ResonanceBurst"), IE_Pressed, this, &ANarisHeroCharacter::ResonanceBurst);
+    Input->BindAction(TEXT("Interact"), IE_Pressed, this, &ANarisHeroCharacter::Interact);
+}
+
+void ANarisHeroCharacter::MoveForward(float Value)
+{
+    if (Controller && Value != 0.f)
+    {
+        const FRotator Rotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+        AddMovementInput(FRotationMatrix(Rotation).GetUnitAxis(EAxis::X), Value);
+    }
+}
+
+void ANarisHeroCharacter::MoveRight(float Value)
+{
+    if (Controller && Value != 0.f)
+    {
+        const FRotator Rotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+        AddMovementInput(FRotationMatrix(Rotation).GetUnitAxis(EAxis::Y), Value);
+    }
+}
+
+void ANarisHeroCharacter::LookYaw(float Value)
+{
+    AddControllerYawInput(Value);
+}
+
+void ANarisHeroCharacter::LookPitch(float Value)
+{
+    AddControllerPitchInput(Value);
+}
+
+void ANarisHeroCharacter::LightAttack()
+{
+    if (Combat)
+    {
+        Combat->AddResonance(5.f);
+    }
+}
+
+void ANarisHeroCharacter::HeavyAttack()
+{
+    if (Combat)
+    {
+        Combat->AddResonance(10.f);
+    }
+}
+
+void ANarisHeroCharacter::Dodge()
+{
+    GetCharacterMovement()->Velocity += GetActorForwardVector() * 650.f;
+}
+
+void ANarisHeroCharacter::Parry()
+{
+    if (Combat)
+    {
+        Combat->AddResonance(15.f);
+    }
+}
+
+void ANarisHeroCharacter::ResonanceBurst()
+{
+    if (Combat && Combat->Resonance >= Combat->MaxResonance)
+    {
+        Combat->Resonance = 0.f;
+    }
+}
+
+void ANarisHeroCharacter::Interact()
+{
+    if (Interaction)
+    {
+        Interaction->Interact();
+    }
+}
+
+void ANarisHeroCharacter::SetSprinting(bool bSprint)
+{
+    GetCharacterMovement()->MaxWalkSpeed = bSprint ? SprintSpeed : WalkSpeed;
+}
