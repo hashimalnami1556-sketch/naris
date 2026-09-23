a65b38ff58fd3828d3bf17a3beebd69e3e2e78ce
hashimalnami1556-sketch <hashimalnami1556@gmail.com>
2026-09-23 16:21:12 +0300

    Implement five-mode Celestial Wolf companion contract

--- unreal/NARIS_W04/Source/NARIS_W04/Public/CelestialWolf.h
+++ unreal/NARIS_W04/Source/NARIS_W04/Public/CelestialWolf.h
@@ -5,6 +5,10 @@
 #include "NarisInteractable.h"
 #include "CelestialWolf.generated.h"
 
+class AActor;
+class ABoneBeastBoss;
+class UNarisPresentationComponent;
+
 UENUM(BlueprintType)
 enum class ENarisWolfMode : uint8
 {
@@ -16,6 +20,11 @@ enum class ENarisWolfMode : uint8
 };
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FNarisWolfBonded);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(
+    FNarisWolfModeChanged,
+    ENarisWolfMode,
+    NewMode
+);
 
 UCLASS(Blueprintable)
 class NARIS_W04_API ACelestialWolf : public ACharacter, public INarisInteractable
@@ -28,14 +37,38 @@ public:
     virtual void BeginPlay() override;
     virtual void Tick(float DeltaSeconds) override;
 
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="NARIS|Presentation")
+    TObjectPtr<UNarisPresentationComponent> Presentation;
+
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Companion")
     ENarisWolfMode Mode = ENarisWolfMode::Guard;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Companion")
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Companion|Movement")
     float FollowDistance = 260.f;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Companion")
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Companion|Movement")
     float FollowSpeed = 560.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Companion|Combat")
+    float AttackAcquireRadius = 1200.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Companion|Combat")
+    float AttackRange = 180.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Companion|Combat")
+    float AttackDamage = 18.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Companion|Combat")
+    float AttackCooldownSeconds = 1.25f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Companion|Track")
+    float TrackAcceptanceRadius = 120.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Companion|EchoLink")
+    float EchoLinkDistance = 320.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Companion|EchoLink")
+    float EchoLinkPulseSeconds = 2.f;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Companion")
     FString CompanionId = TEXT("CelestialWolf");
@@ -52,8 +85,14 @@ public:
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Companion")
     bool bBonded = false;
 
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Companion|Track")
+    TObjectPtr<AActor> TrackTarget = nullptr;
+
     UPROPERTY(BlueprintAssignable, Category="Companion")
     FNarisWolfBonded OnBonded;
+
+    UPROPERTY(BlueprintAssignable, Category="Companion")
+    FNarisWolfModeChanged OnModeChanged;
 
     UFUNCTION(BlueprintCallable, Category="NARIS|Companion")
     void SetMode(ENarisWolfMode NewMode);
@@ -62,8 +101,24 @@ public:
     void EchoLink();
 
     UFUNCTION(BlueprintCallable, Category="NARIS|Companion")
+    void SetTrackTarget(AActor* NewTarget);
+
+    UFUNCTION(BlueprintCallable, Category="NARIS|Companion")
     bool BondWithPlayer(AActor* InstigatorActor);
 
     virtual bool Interact_Implementation(AActor* InstigatorActor) override;
     virtual FText GetInteractionPrompt_Implementation() const override;
+
+private:
+    FVector GuardAnchor = FVector::ZeroVector;
+    float LastAttackTime = -1000.f;
+    float LastEchoPulseTime = -1000.f;
+
+    void MoveTowards(const FVector& Destination, float AcceptanceRadius);
+    ABoneBeastBoss* FindAttackTarget() const;
+    void TickFollow();
+    void TickGuard();
+    void TickAttack();
+    void TickTrack();
+    void TickEchoLink();
 };
