748af85b5831f92a768f7870ff2a3436c53f6911
hashimalnami1556-sketch <hashimalnami1556@gmail.com>
2026-09-23 15:34:24 +0300

    Add native NARIS runtime smoke HUD

---
+++ unreal/NARIS_W04/Source/NARIS_W04/Public/NarisHUD.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "NarisHUD.generated.h"
+
+UCLASS(Blueprintable)
+class NARIS_W04_API ANarisHUD : public AHUD
+{
+    GENERATED_BODY()
+
+public:
+    virtual void DrawHUD() override;
+
+private:
+    void DrawBar(
+        const FString& Label,
+        float Value,
+        float MaxValue,
+        float X,
+        float Y,
+        const FLinearColor& FillColor
+    );
+};
