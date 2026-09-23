1b16205a302a9800f3392f2a9fb0e29c4e2e02e2
hashimalnami1556-sketch <hashimalnami1556@gmail.com>
2026-09-23 15:03:05 +0300

    Restrict interaction to NARIS interactables

--- unreal/NARIS_W04/Source/NARIS_W04/Public/NarisInteractionComponent.h
+++ unreal/NARIS_W04/Source/NARIS_W04/Public/NarisInteractionComponent.h
@@ -1,15 +1,28 @@
 #pragma once
+
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "NarisInteractionComponent.generated.h"
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FNarisInteractEvent,AActor*,Actor);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FNarisInteractEvent, AActor*, Actor);
+
 UCLASS(ClassGroup=(NARIS), Blueprintable, meta=(BlueprintSpawnableComponent))
 class NARIS_W04_API UNarisInteractionComponent : public UActorComponent
 {
     GENERATED_BODY()
+
 public:
     UNarisInteractionComponent();
-    UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Interaction") float Radius=220.f;
-    UPROPERTY(BlueprintAssignable) FNarisInteractEvent OnInteracted;
-    UFUNCTION(BlueprintCallable,Category="NARIS|Interaction") bool Interact();
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Interaction")
+    float Radius = 220.f;
+
+    UPROPERTY(BlueprintAssignable)
+    FNarisInteractEvent OnInteracted;
+
+    UFUNCTION(BlueprintCallable, Category="NARIS|Interaction")
+    bool Interact();
+
+    UFUNCTION(BlueprintPure, Category="NARIS|Interaction")
+    AActor* FindNearestInteractable() const;
 };
