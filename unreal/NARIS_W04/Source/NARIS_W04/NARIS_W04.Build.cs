ba3927965330a4cc0af57a6c46ed118a97183ae2
hashimalnami1556-sketch <hashimalnami1556@gmail.com>
2026-09-23 15:56:43 +0300

    Add Json dependency for packaged runtime smoke reporting

--- unreal/NARIS_W04/Source/NARIS_W04/NARIS_W04.Build.cs
+++ unreal/NARIS_W04/Source/NARIS_W04/NARIS_W04.Build.cs
@@ -8,7 +8,7 @@ public class NARIS_W04 : ModuleRules
         PublicDependencyModuleNames.AddRange(new string[]
         {
             "Core","CoreUObject","Engine","GameplayTags","EnhancedInput",
-            "AIModule","NavigationSystem","UMG"
+            "AIModule","NavigationSystem","UMG","Json"
         });
     }
 }
