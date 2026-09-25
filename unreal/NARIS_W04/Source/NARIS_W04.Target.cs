a6ebff3d00cb35e8c1ab2e42c5da33e1fcf14cda
hashimalnami1556-sketch <hashimalnami1556@gmail.com>
2026-09-25 04:55:37 +0300

    Fix W04 Unreal 5.7 editor build compatibility

--- unreal/NARIS_W04/Source/NARIS_W04.Target.cs
+++ unreal/NARIS_W04/Source/NARIS_W04.Target.cs
@@ -6,8 +6,8 @@ public class NARIS_W04Target : TargetRules
     public NARIS_W04Target(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Game;
-        DefaultBuildSettings = BuildSettingsVersion.V5;
-        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+        DefaultBuildSettings = BuildSettingsVersion.V6;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
         ExtraModuleNames.Add("NARIS_W04");
     }
 }
